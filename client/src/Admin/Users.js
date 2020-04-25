07d650858266d6e2a7835a6230a25682068a65aa
AnthonyTC89 <ptonyptc@gmail.com>
2020-04-24 21:28:14 -0500

    User Admin Component styled

--- client/src/Admin/Users.js
+++ client/src/Admin/Users.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 import UsersForm from '../Components/UsersForm';
 import './Users.css';
 
@@ -62,21 +63,40 @@ class Users extends React.Component {
         <h2>usuarios</h2>
         {message === null ? null : <p className="text-success">{message}</p>}
         {error === null ? null : <p className="text-danger">{error}</p>}
+        <div className="row row-user">
+          <div className="col-2 user-id">id</div>
+          <div className="col-2 user-username">username</div>
+          <div className="col-4 user-email">email</div>
+          <div className="col-2 user-status">status</div>
+          <div className="col-2 btn-actions">actions</div>
+        </div>
         {formVisible ? <UsersForm user={userEdit} />
           : users.map((user) => (
-            <div className="row">
-              <p>{user.id}</p>
-              <p>{user.username}</p>
-              <button
-                className="btn btn-success"
-                type="button"
-                onClick={() => this.handleOpenUserForm(user)}
-              >
-                Edit
-              </button>
-              <button className="btn btn-danger" type="button" disabled={loading}>
-                Eliminar
-              </button>
+            <div key={uuidv4()} className="row row-user">
+              <div className="col-2 user-id">
+                <p>{user.id}</p>
+              </div>
+              <div className="col-2 user-username">
+                <p>{user.username}</p>
+              </div>
+              <div className="col-4 user-email">
+                <p>{user.email}</p>
+              </div>
+              <div className="col-2 user-status">
+                <p>{user.status}</p>
+              </div>
+              <div className="col-2 btn-actions">
+                <button
+                  className="btn btn-success"
+                  type="button"
+                  onClick={() => this.handleOpenUserForm(user)}
+                >
+                  Edit
+                </button>
+                <button className="btn btn-danger" type="button" disabled={loading}>
+                  Eliminar
+                </button>
+              </div>
             </div>
           ))}
       </section>
